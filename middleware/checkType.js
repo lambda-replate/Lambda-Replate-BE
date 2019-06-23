3c68ae29a22d60e8d6922a9e41961eeb2bb781a3
josh182014 <josh182014@gmail.com>
2019-06-23 16:01:19 -0600

    added middleware to check type of account

---
+++ middleware/checkType.js
@@ -0,0 +1,19 @@
+module.exports = type => {
+    return (req, res, next) => {
+        if (req.user) {
+            console.log(req.user)
+            if (
+                req.user.type &&
+                req.user.type.includes(type)
+                ) {
+                next()
+            }
+            else {
+                res.status(401).json('Not Authorized')
+            }
+        }
+        else {
+            res.status(401).json('Not Authorized')
+        }
+    }
+}
