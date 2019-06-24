33e0faeef79c912da783aa38034c550a85509bf3
josh182014 <josh182014@gmail.com>
2019-06-24 12:56:46 -0600

    debugging seed not working

--- data/seeds/001-volunteers.js
+++ data/seeds/001-volunteers.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('volunteers').truncate()
+  return knex('volunteers').del()
     .then(function () {
       // Inserts seed entries
       return knex('volunteers').insert([
