a80625e7088b2216cf6702d67e0899201a962452
josh182014 <josh182014@gmail.com>
2019-06-25 11:14:29 -0600

    removed console log

--- food/food-router.js
+++ food/food-router.js
@@ -51,7 +51,6 @@ router.post('/', (req, res) => {
         res.status(200).json(response)
     })
     .catch(error => {
-        console.log(error)
         res.status(500).json("Server Error")
     })
 })
