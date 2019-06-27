da0bb46af7fcf5ccb1cb991f7284c3c441b0e973
josh182014 <josh182014@gmail.com>
2019-06-27 13:43:21 -0600

    testing new cleanup file

--- data/seeds/000-cleanup.js
+++ data/seeds/000-cleanup.js
@@ -1,8 +1,16 @@
-const cleaner = require('knex-cleaner');
+// const cleaner = require('knex-cleaner');
+
+// exports.seed = knex => {
+//     return cleaner.clean(knex, {
+//         ignoreTables: ['knex_migrations', 'knex_migrations_lock'],
+//         mode: 'truncate'
+//     });
+// };
 
 exports.seed = knex => {
-    return cleaner.clean(knex, {
-        ignoreTables: ['knex_migrations', 'knex_migrations_lock'],
-        mode: 'truncate'
-    });
-};
+    return knex('food').truncate().then(function() {
+        return knex('volunteers').truncate().then(function() {
+            return knex('businesses').truncate()
+        })
+    })
+}
