0f400bd6c6c4b467c96b1924d6d63828b1ec7155
josh182014 <josh182014@gmail.com>
2019-06-27 12:37:27 -0600

    changed from truncate to delete

--- data/seeds/002-volunteers.js
+++ data/seeds/002-volunteers.js
@@ -2,7 +2,7 @@ const bcrypt = require('bcryptjs')
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('volunteers').truncate()
+  return knex('volunteers').del()
     .then(function () {
       // Inserts seed entries
       return knex('volunteers').insert([
