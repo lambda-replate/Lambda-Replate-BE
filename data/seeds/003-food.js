11d846aea5a12bbe12728b95834cf77640bb91f6
josh182014 <josh182014@gmail.com>
2019-06-27 14:11:23 -0600

    updated seed time to match frontend

--- data/seeds/003-food.js
+++ data/seeds/003-food.js
@@ -1,8 +1,8 @@
 
 exports.seed = function(knex, Promise) {
       return knex('food').insert([
-        {name: 'meatballs', time: 12345, description: 'This is a description for meatballs. Come pick them up whenever.', is_claimed: 1, business_id: 1, volunteer_id: 2, pickup_date: '2019-06-28'},
-        {name: 'pizza', time: 12345, description: 'This is a description for our pizza. Come pick it up whenever.', is_claimed: 1, business_id: 1, volunteer_id: 3, pickup_date: '2019-06-27'},
-        {name: 'soup', time: 12345, description: 'We have a ton of soup!', is_claimed: 1, business_id: 3, volunteer_id: 1, pickup_date: '2019-06-28'},
+        {name: 'meatballs', time: '10:05', description: 'This is a description for meatballs. Come pick them up whenever.', is_claimed: 1, business_id: 1, volunteer_id: 2, pickup_date: '2019-06-28'},
+        {name: 'pizza', time: '9:30', description: 'This is a description for our pizza. Come pick it up whenever.', is_claimed: 1, business_id: 1, volunteer_id: 3, pickup_date: '2019-06-27'},
+        {name: 'soup', time: '6:00', description: 'We have a ton of soup!', is_claimed: 1, business_id: 3, volunteer_id: 1, pickup_date: '2019-06-28'},
       ]);
 };
