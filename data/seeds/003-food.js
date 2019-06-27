99ae25b3c89adda525ee7756fb4e0f3d16e1642d
josh182014 <josh182014@gmail.com>
2019-06-27 12:30:38 -0600

    added knex cleaner and changed seeds to truncate instead of delete

--- data/seeds/003-food.js
+++ data/seeds/003-food.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('food').del()
+  return knex('food').truncate()
     .then(function () {
       // Inserts seed entries
       return knex('food').insert([
